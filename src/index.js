68914b007f522a0adfed7c38310077057956fd36
selinabauer <124388794+selinabauer@users.noreply.github.com>
2024-06-01 15:06:25 +0200

    installed the API

--- src/index.js
+++ src/index.js
@@ -1,9 +1,25 @@
+function refreshWheater(response) {
+  let temperature = document.querySelector("#wheater-app-temperture-value");
+  let actualTemperature = response.data.temperature.current;
+  let newCity = document.querySelector("#wheater-app-city");
+  newCity.innerHTML = response.data.city;
+
+  temperature.innerHTML = Math.round(actualTemperature);
+}
+
+function searchCity(city) {
+  let apiKey = "f4t0o3b00c8eabf0c1910e4852f5c3b5";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(refreshWheater);
+}
+
 function callInputValue(event) {
   event.preventDefault();
-  let city = document.querySelector("#search-form-input");
-  let newCity = document.querySelector("#wheater-app-city");
-  newCity.innerHTML = city.value;
+  let currentCity = document.querySelector("#search-form-input");
+
+  searchCity(currentCity.value);
 }
 
 let inputValue = document.querySelector("#search-form");
 inputValue.addEventListener("submit", callInputValue);
+searchCity("Bern");
